4bf6f53ee5c4ee575c795da83191513618b679fd
lprates05 <lorenapratesdl@outlook.com>
2025-05-02 01:55:30 +0000

    movies

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,8 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors
+    render ({:template => "misc_templates/directors"})
+  end
 end
