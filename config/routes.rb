c259abaf2b14d07fe24b0f5d50b9c085fe9fbab6
lprates05 <lorenapratesdl@outlook.com>
2025-05-04 16:52:01 +0000

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get("/directors", {:controller => "directors", :action => "index"})
 
   get("/directors/:the_id", {:controller => "directors", :action => "details"})
+
+  get("/movies", {:controller => "movies", :action => "index"})
+
+  get("/movies/:the_id", {:controller => "movies", :action => "details"})
 end
